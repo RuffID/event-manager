71a3d0a587aef744c1a649b23003f832aec11e11
Danil Parshakov <enqtk@protonmail.com>
2026-08-03 16:23:59 +0700

    docs: document event API with Swagger
    Enable XML documentation generation and configure Swagger to display API comments.
    Describe endpoint responses and supported content types.
    Add concise setup instructions and endpoint documentation to the README.

--- EventManager.Api/Controllers/EventsController.cs
+++ EventManager.Api/Controllers/EventsController.cs
@@ -11,13 +11,15 @@ namespace EventManager.Api.Controllers
     /// <param name="eventService">Сервис для работы с событиями.</param>
     [Route("api/[controller]")]
     [ApiController]
+    [Produces("application/json")]
     public class EventsController(IEventService eventService) : ControllerBase
     {
         /// <summary>
         /// Возвращает список всех событий.
         /// </summary>
-        /// <returns>HTTP-ответ со списком событий или описанием ошибки.</returns>
+        /// <response code="200">Возвращает список событий.</response>
         [HttpGet]
+        [ProducesResponseType(typeof(List<EventDto>), StatusCodes.Status200OK)]
         public IActionResult GetEvents()
         {
             ServiceResult<List<EventDto>> result = eventService.GetEvents();
@@ -32,8 +34,11 @@ namespace EventManager.Api.Controllers
         /// Возвращает событие по идентификатору.
         /// </summary>
         /// <param name="id">Идентификатор события.</param>
-        /// <returns>HTTP-ответ с событием или описанием ошибки.</returns>
+        /// <response code="200">Возвращает найденное событие.</response>
+        /// <response code="404">Событие с указанным идентификатором не найдено.</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(typeof(EventDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ServiceError), StatusCodes.Status404NotFound)]
         public IActionResult GetEvent(Guid id)
         {
             ServiceResult<EventDto> result = eventService.GetEventById(id);
@@ -48,8 +53,14 @@ namespace EventManager.Api.Controllers
         /// Создаёт новое событие.
         /// </summary>
         /// <param name="dto">Данные создаваемого события.</param>
-        /// <returns>HTTP-ответ с созданным событием или описанием ошибки.</returns>
+        /// <response code="201">Возвращает созданное событие.</response>
+        /// <response code="400">Переданы невалидные данные события.</response>
+        /// <response code="500">Не удалось сохранить событие.</response>
         [HttpPost]
+        [Consumes("application/json")]
+        [ProducesResponseType(typeof(EventDto), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ServiceError), StatusCodes.Status500InternalServerError)]
         public IActionResult CreateEvent([FromBody] CreateEventDto dto)
         {
             ServiceResult<EventDto> result = eventService.CreateEvent(dto);
@@ -65,8 +76,14 @@ namespace EventManager.Api.Controllers
         /// </summary>
         /// <param name="id">Идентификатор обновляемого события.</param>
         /// <param name="dto">Новые данные события.</param>
-        /// <returns>HTTP-ответ с обновлённым событием или описанием ошибки.</returns>
+        /// <response code="200">Возвращает обновлённое событие.</response>
+        /// <response code="400">Переданы невалидные данные события.</response>
+        /// <response code="404">Событие с указанным идентификатором не найдено.</response>
         [HttpPut("{id}")]
+        [Consumes("application/json")]
+        [ProducesResponseType(typeof(EventDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ServiceError), StatusCodes.Status404NotFound)]
         public IActionResult UpdateEvent(Guid id, [FromBody] UpdateEventDto dto)
         {
             ServiceResult<EventDto> result = eventService.UpdateEvent(id, dto);
@@ -81,8 +98,11 @@ namespace EventManager.Api.Controllers
         /// Удаляет событие по идентификатору.
         /// </summary>
         /// <param name="id">Идентификатор удаляемого события.</param>
-        /// <returns>HTTP-ответ без тела или описание ошибки.</returns>
+        /// <response code="204">Событие успешно удалено. Тело ответа отсутствует.</response>
+        /// <response code="404">Событие с указанным идентификатором не найдено.</response>
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ServiceError), StatusCodes.Status404NotFound)]
         public IActionResult DeleteEvent(Guid id)
         {
             ServiceResult<bool> result = eventService.DeleteEvent(id);
